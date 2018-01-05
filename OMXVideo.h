d86822b44ddcf021d19e33073104a3b18d7bd1fa
colinporth <colinporth@users.noreply.github.com>
2018-01-05 18:29:47 +0000

    fidle

--- OMXVideo.h
+++ OMXVideo.h
@@ -10,7 +10,7 @@
 #include "OMXClock.h"
 #include "OMXReader.h"
 
-#include "guilib/Geometry.h"
+#include "Geometry.h"
 #include "utils/SingleLock.h"
 
 #define VIDEO_BUFFERS 60
--- omxplayer.cpp
+++ omxplayer.cpp
@@ -1,23 +1,4 @@
-//{{{
-/*
- *
- *      Copyright (C) 2012 Edgar Hucek
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
-//}}}
+// omxplayer.cpp
 //{{{  includes
 #include <stdio.h>
 #include <signal.h>
@@ -45,6 +26,7 @@ extern "C" {
 #include "DllAvUtil.h"
 #include "DllAvFormat.h"
 #include "DllAvCodec.h"
+
 #include "linux/RBP.h"
 
 #include "OMXVideo.h"
@@ -58,7 +40,6 @@ extern "C" {
 #include "OMXPlayerSubtitles.h"
 #include "DllOMX.h"
 
-#include "Srt.h"
 #include "KeyConfig.h"
 #include "utils/Strprintf.h"
 #include "Keyboard.h"
